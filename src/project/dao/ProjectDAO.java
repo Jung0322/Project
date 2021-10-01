5de0ca016aca83dd258fd28868c7266d0528e99b
simon <ghtmdaks@naver.com>
2021-10-01 18:17:25 +0900

    충돌

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -1,5 +1,24 @@
 package project.dao;
 
 public class ProjectDAO {
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDrivet");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	} 
 }
