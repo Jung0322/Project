9f44b5ddd0a757ed9095e248c7768e1e245efd56
jung <3dnr22@gmail.com>
2021-10-05 10:00:52 +0900

    getConnection

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -1,15 +1,30 @@
 package project.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ProjectDAO {
 
-	
 	static {
 		try {
 			Class.forName("oracle.jdbc.OracleDriver");
-		}catch(ClassNotFoundException e){
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
+	public static Connection getConnection() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "c##scott";
+		String password = "tiger";
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 }
